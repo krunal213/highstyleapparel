7163fdab06f8f422b26506adcae32fbd1abb7e94
krunal21041993 <kathikar.krunal3@gmail.com>
2025-02-08 18:23:48 +0530

    log in screen ui #12
    completed the following
    
    added ui
    added navigation for
    
    1.log in
    2.sign up
    3.verification code
    4.forgot password
    5.create new password
    
    customize appbar

--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -2,19 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:highstyleapparel/navigations.dart';
 
 class Splash extends StatefulWidget {
-  const Splash({Key? key}) : super(key: key);
+  const Splash({super.key});
 
   @override
   State<Splash> createState() => _SplashState();
 }
 
 class _SplashState extends State<Splash> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(children: [
-        Image(
+        const Image(
           key: Key("image_widget_background"),
           image: AssetImage('assets/images/ic_welcome_background.png'),
           fit: BoxFit.cover,
@@ -23,7 +22,7 @@ class _SplashState extends State<Splash> {
           alignment: Alignment.center,
         ),
         Opacity(
-            key: Key("opacity_widget"),
+            key: const Key("opacity_widget"),
             opacity: 0.85,
             child: Container(
               color: Colors.black,
@@ -35,57 +34,62 @@ class _SplashState extends State<Splash> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             mainAxisSize: MainAxisSize.min,
             children: [
-              Image(
+              const Image(
                 key: Key("image_widget_icon"),
                 image: AssetImage('assets/images/ic_high_style.png'),
                 width: 254,
                 height: 216,
               ),
-              Text(
-                  "Welcome to \n High Style Apparel",
+              const Text("Welcome to \n High Style Apparel",
                   key: Key("text_widget_welcome_message"),
                   style: TextStyle(
                       fontSize: 24.0,
                       fontWeight: FontWeight.bold,
                       color: Colors.white),
                   textAlign: TextAlign.center),
-              SizedBox(height: 2),
-              Text("The home for a highstyleapparel",
+              const SizedBox(height: 2),
+              const Text("The home for a highstyleapparel",
                   key: Key("text_widget_slogan"),
                   style: TextStyle(fontSize: 16.0, color: Colors.white),
                   textAlign: TextAlign.center),
-              SizedBox(height: 16),
+              const SizedBox(height: 16),
               OutlinedButton(
-                  key: Key("outlinebutton_widget_login"),
+                  key: const Key("outlinebutton_widget_login"),
                   style: OutlinedButton.styleFrom(
-                      backgroundColor: Color(0xFF444343),
-                      minimumSize: Size(164, 48),
-                      side: BorderSide(
-                        color: Colors.white, // Border color
-                        width: 1.18, // Stroke size (thickness)
-                      )),
+                          backgroundColor: Color(0xFF444343),
+                          minimumSize: Size(164, 48),
+                          side: const BorderSide(
+                            color: Colors.white, // Border color
+                            width: 1.18, // Stroke size (thickness)
+                          ))
+                      .copyWith(
+                          overlayColor: MaterialStateProperty.all(
+                              Colors.white.withOpacity(0.2))),
                   onPressed: () {
                     LoginRoute().push(context);
                   },
-                  child: Text("LOG IN",
+                  child: const Text("LOG IN",
                       style: TextStyle(
                           fontSize: 16.0,
                           fontWeight: FontWeight.bold,
                           color: Colors.white))),
-              SizedBox(height: 16),
+              const SizedBox(height: 16),
               OutlinedButton(
-                key: Key("outlinebutton_widget_signup"),
+                key: const Key("outlinebutton_widget_signup"),
                 style: OutlinedButton.styleFrom(
-                    backgroundColor: Color(0xFF444343),
-                    minimumSize: Size(164, 48),
-                    side: BorderSide(
-                      color: Colors.white, // Border color
-                      width: 1.18, // Stroke size (thickness)
-                    )),
+                        backgroundColor: const Color(0xFF444343),
+                        minimumSize: const Size(164, 48),
+                        side: const BorderSide(
+                          color: Colors.white, // Border color
+                          width: 1.18, // Stroke size (thickness)
+                        ))
+                    .copyWith(
+                        overlayColor: MaterialStateProperty.all(
+                            Colors.white.withOpacity(0.3))),
                 onPressed: () {
                   SignUpRoute().push(context);
                 },
-                child: Text("SIGN UP",
+                child: const Text("SIGN UP",
                     style: TextStyle(
                         fontSize: 16.0,
                         fontWeight: FontWeight.bold,
