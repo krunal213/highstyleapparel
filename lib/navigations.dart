2ce46c484c39e6bfa9196255478806cbde1bf033
krunal21041993 <kathikar.krunal3@gmail.com>
2025-02-08 13:17:23 +0530

    setup dependencies for project #3

---
+++ lib/navigations.dart
@@ -0,0 +1,7 @@
+import 'package:go_router/go_router.dart';
+
+part 'navigations.g.dart';
+//https://pub.dev/packages/go_router
+//https://github.com/flutter/packages/blob/main/packages/go_router_builder/example/lib/simple_example.dart
+//https://pub.dev/documentation/go_router/latest/topics/Type-safe%20routes-topic.html
+
